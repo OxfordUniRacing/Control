52fa86bad81c41bbd36bfd45a6bdcb09fe62d11a
univ4123 <univ4123@engs-22438.eng.ox.ac.uk>
2019-01-24 16:01:15 +0000

    Major Update: Tire Force Observer made into a MIMO System so that only one model is needed The logic that controls when to call the Control System has been put into its own model called Top Level System
    Some other minor updates were also made like cleaning up the Rate Tranisitons and putting a memory block on the algebraic loop in the testbench to prevent errors

--- Parameters.m
+++ Parameters.m
@@ -29,20 +29,47 @@ reduction = 0.9;
 %Sample time
 TFO_Ts = 0.01;
 
-% observer matrices
 
-A = [0 -1/Jw; 0 0];
-B = [R/Jw; 0];
-C = [1 0];
+% observer matrices SISO
 
-% observer gains
+A_s = [0 -1/Jw; 0 0];
+B_s = [R/Jw; 0];
+C_s = [1 0];
+
+% observer gains SISO
 
 l1 = 5;
 l2 = 10;
 
-K = [l1; -l2];
+K_s = [l1; -l2];
+
+% observer matrices MIMO
+
+A = [A_s zeros(2); zeros(2) A_s];
+B = [B_s zeros(2,1); zeros(2,1) B_s];
+C = [C_s zeros(1,2); zeros(1,2) C_s];
+
+% observer gains MIMO
+
+l1 = 5;
+l2 = 10;
+
+K = [K_s zeros(2,1); zeros(2,1) K_s];
+
 
 %% Slip Ratio Controller
 
 %Sample time
 SRC_Ts = 0.1;
+
+%% C code settings
+
+try
+set_param('Optimal_Slip_Estimator','TargetLangStandard','C99 (ISO)');
+set_param('Slip_Ratio_Controller','TargetLangStandard','C99 (ISO)');
+set_param('Tire_Force_Observer','TargetLangStandard','C99 (ISO)');
+set_param('Control_System','TargetLangStandard','C99 (ISO)');
+set_param('Control_System_Testbench','TargetLangStandard','C99 (ISO)');
+catch
+    disp('Models not loaded yet, compile simulink');
+end
